52e747630471ef9dd980590c1c5931797c80864b
raynamharris <rayna.harris@utexas.edu>
2017-03-14 18:02:10 -0500

    better figures

--- markdownfiles/DESeqPCAfunction.R
+++ markdownfiles/DESeqPCAfunction.R
@@ -36,11 +36,13 @@ plotPC1PC2 <- function(aescolor, colorname, aesshape, shapename, colorvalues){
     #stat_ellipse(level = 0.95, (aes(color=aescolor)),size=1) + 
     scale_colour_manual(name=colorname, values=c(colorvalues))+
     scale_shape_discrete(name=shapename) +
-    theme(axis.text = element_text(size=14),
-          axis.title.x = element_text(size=16),
-          axis.title.y = element_text(size=16),
-          legend.title = element_text(size=16),
-          legend.text = element_text(size=14))
+    theme(axis.text = element_text(size=12),
+          axis.title.x = element_text(size=12),
+          axis.title.y = element_text(size=12),
+          legend.title = element_text(size=12, face="bold"),
+          legend.text = element_text(size=12)) + 
+    guides(colour = guide_legend(override.aes = list(shape = 15), order = 1),
+           shape = guide_legend(order = 2))
 }
 
 
--- markdownfiles/figureoptions.R
+++ markdownfiles/figureoptions.R
@@ -1,6 +1,16 @@
 ## this R script loads some themes for ggplots
 
-## should be set as 'ann_colors' to be called in all pheatmapts
+# color blind safe red blue green -ish colors
+# orange #d95f02 - DG
+# purple #7570b3 - CA1
+# green #1b9e77  - CA3
+
+## for the pca plot
+colorvalRegion <- c("#7570b3", "#1b9e77", "#d95f02")
+colorvalMethod <- c("#969696", "#525252")
+colorvalGroup <- c("#969696", "#525252")
+
+## pheatmapt useage: set 'ann_colors' to be one of these
 ann_colorsdissociation = list(Method = c(control = (values=c("#969696")), 
                               dissociated = (values=c("#525252"))),
                   Region = c(CA1 = (values=c("#7570b3")),
@@ -19,34 +29,12 @@ ann_colorsbehavior = list(Group = c(control = (values=c("#969696")),
                                    CA3 = (values=c("#1b9e77")), 
                                    DG = (values=c("#d95f02"))))
 
-
-
 ann_colorscembrowksi = list(Location = c(dorsal = (values=c("#525252")),
                                          ventral = (values=c("#969696"))),
                   Region =  c(CA1 = (values=c("#7570b3")),  
                               CA3 = (values=c("#1b9e77")), 
                               DG = (values=c("#d95f02"))))
 
-ann_colorscombo = list(exp = c(cembrowski = (values=c("#969696")), 
-                               harris = (values=c("#525252"))),
-                       Region = c(CA1 = (values=c("#7570b3")),
-                                  CA3 = (values=c("#1b9e77")), 
-                                  DG = (values=c("#d95f02"))))
-
-
-# for the heatmap palette. call with 'colorpalette'
+# for the pheatmap palette. call with 'colorpalette'
 matlabcolors <-  matlab.like2(100)  #color schelem
 cembrowskicolors <-  colorRampPalette(c("Deep Sky Blue 3", "white", "red"))( 30 )
-
-
-
-## for the pca plot
-colorvalRegion <- c("#7570b3", "#1b9e77", "#d95f02")
-colorvalMethod <- c("#969696", "#525252")
-colorvalGroup <- c("#969696", "#525252")
-colorvalnewgroup <- c("#969696", "#525252", "#7570b3")
-
-# color blind safe red blue green -ish colors
-# orange #d95f02
-# purple #7570b3
-# green #1b9e77
