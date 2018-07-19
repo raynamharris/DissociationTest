52d3058ed6f9aa604b22bcb87f9eb42775043dab
raynamharris <rayna.harris@utexas.edu>
2018-07-19 15:22:15 -0700

    update install list

--- install.R
+++ install.R
@@ -1,9 +1,5 @@
-# script to install R packages needed for pipeline
-# must be run from analysis_pipeline directory
-# argument to script is path to R library for install
-
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args) > 0) .libPaths(args[1])
-
-
-install.packages(c("highr", "caTools", "knitr", "rmarkdown", "dplyr", "plyr", "reshape2"), repos=NULL, type="source")
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
